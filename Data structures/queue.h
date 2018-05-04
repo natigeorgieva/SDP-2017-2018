56579da9cda7d688004e7e12917b7ed0e2df9717
maksimsdimov <maksim.s.dimov@gmail.com>
2018-05-04 18:58:03 +0300

    Update queue.h

--- Data structures/queue.h
+++ Data structures/queue.h
@@ -12,9 +12,9 @@ public:
 	~queue();
 	bool isEmpty() const; //връща дали е празен
 	int size() const; //връща текущия размер
-	void push(int val); //вкарва елемент "най-отгоре"/отпред
-	void pop(); //изтрива "най-горния"/последния вкаран елемент
-	int front(); //показва "най-горния"/първия вкаран елемент
-	int back(); //показва "най-долния"/последния вкаран елемент
+	void push(int val); //вкарва елемент отпред
+	void pop(); //изтрива последния вкаран елемент
+	int front(); //показва елементa най-отпред
+	int back(); //показва елемента най-отзад
 	friend std::ostream &operator<< (ostream &output, const dynamicStack &other);
 };
